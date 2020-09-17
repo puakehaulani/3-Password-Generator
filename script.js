7adacccc90fb2c4eee06e7b4329de021f97b4375
puakehaulani <puakehaulani@gmail.com>
2020-09-17 13:55:06 -0700

    added back in prompts and confirms into function, added conditions around prompt, cleaned up comments

--- script.js
+++ script.js
@@ -12,69 +12,59 @@
 // * Gather user input with prompt's and confirm's
 
 //////////////////////////////////////
-//MYCODE
-
-//make it so prompts dont show til button is clicked
-
-//Prompt for length of password 8-128 x
-// var passwordLength = prompt("Choose a password length of 8-128 characters")
-var passwordLength = 10
-//user to enter number
-//store response in a variable x
-
-//recognize it as a number
-// if (isNAN(passwordLength)) {
-//     alert(passwordLength + " is not a number");
-// } else(passwordLength >= 129 | passwordLength <= 7) {
-//     alert("Please enter a number between 8 and 128")}
 
 
-//Prompt for using lowercase letters x
-// var confirmLowercase = confirm("Does your password have lowercase letters?")
-var confirmLowercase = true
-//user to enter ok or cancel (boolean) x
-//store response in a variable x
-
-
-//Prompt for using uppercase letters x
-// var confirmUppercase = confirm("Does your password have uppercase letters?")
-var confirmUppercase = true
-//user to enter ok or cancel (boolean) x
-//store response in a variable x
-
-
-//Prompt for using numeric characters x
-// var confirmNumber = confirm("Does your password have numbers?")
-var confirmNumber = true
-//store response in a variable x
-
-
-//Prompt for using special characters x
-// var confirmSpecial = confirm("Does your password have special characters?")
-var confirmSpecial = true
-//user to enter ok or cancel (boolean) x
-//store response in a variable x
-
-
-//Validate all responses 
-//make sure at least one is clicked as ok
-
-
+//constants
 let characters = "abcdefghijklmnopqrstuvwxyz"
 let special = "!@#$%^&*.,"
 
+
+//helper function
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 
+function getPasswordLength(text) {
+    let response = prompt(text)
+    if (isNaN(response)) {
+        alert("Enter a number between 8-128")
+        response = 0
+    }
+    return response
+}
+
 function generatePassword() {
     let password = "";
+    //Make it so prompts dont show til button is clicked
+    //Prompt for length of password 8-128 user enters number prompt stores in passwordLength variable
+    var passwordLength = 0
+    while (passwordLength < 8 || passwordLength > 128) {
+        passwordLength = getPasswordLength("Choose a password length of 8-128")
+    }
+
+    //Prompt for using lowercase letters user chooses ok/cancel boolean stores in confirmLowercase variable
+    var confirmLowercase = confirm("Does your password have lowercase letters?")
+
+    //Prompt for using uppercase letters user chooses ok/cancel boolean stores in confirmUppercase variable
+    var confirmUppercase = confirm("Does your password have uppercase letters?")
+
+    //Prompt for using numeric characters user chooses ok/cancel boolean stores in confirmNumber variable
+    var confirmNumber = confirm("Does your password have numbers?")
+
+    //Prompt for using special characters user chooses ok/cancel boolean stores in confirmSpecial variable
+    var confirmSpecial = confirm("Does your password have special characters?")
+
+    //Make sure at least one type is chosen ok
+    //If not alert and go back
+
+    //Create constants from user input
     let needsNumber = confirmNumber
     let needsLowercase = confirmLowercase
     let needsUppercase = confirmUppercase
     let needsSpecial = confirmSpecial
-    //generate random password character by character (i++)
+
+    //Generate random password character by character (i++)
     for (var i = 0; password.length < passwordLength; i++) {
         let randomChoice = randomNumber(0, 4)
         console.log("creating character " + randomChoice)
@@ -106,8 +96,7 @@ function generatePassword() {
             needsSpecial = false
             continue
         }
-        //actually add the character, not just console.log
-        //choose random character type, not the same one each time
+        //second run
         if (randomChoice === 0 && confirmNumber) {
             let c = randomNumber(0, 10)
             password = password + c
@@ -126,17 +115,18 @@ function generatePassword() {
             console.log("i'm adding " + c);
             continue
         }
-        if (randomChoice === 3 && confirmNumber) {
+        if (randomChoice === 3 && confirmSpecial) {
             let c = special[randomNumber(0, special.length)]
             password = password + c
             console.log("i'm adding " + c);
             continue
         }
     }
+    //Display on page
     return password;
 }
 
-//display on page
+
 
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
